c581439e927a367c07f5e51161bb1c940399e446
linuxra <54610196+linuxra@users.noreply.github.com>
2023-07-24 11:56:06 -0400

    test

---
+++ settings3.py
@@ -0,0 +1,11 @@
+
+
+
+
+settings = {'PATH': {'RAI_PTH': '/testpath/var'},
+ 'report1': {'df': 'df1',
+             'files': {'csv': 'filename.cvs', 'pdf': 'filename.pdf'},
+             'name': 'Value'},
+ 'report2': {'df': 'df1',
+             'files': {'csv': 'filename.cvs', 'pdf': 'filename.pdf'},
+             'name': 'Value'}}
---
+++ settings6.py
@@ -0,0 +1,14 @@
+colors = 'red, blue'
+onecolor = 'white'
+path = '/users/modeling/'
+path1 = '/users/modeling/'
+
+settings = {'PATH': {'RAI_PTH': {'/testpath/var': {'/testpath/var': nan}}},
+ 'report1': {'df': {'df1': {'df1': nan}},
+             'files': {'csv': {'csv': 'filename.cvs'},
+                       'pdf': {'pdf': 'filename.pdf'}},
+             'name': {'Value': {'Value': nan}}},
+ 'report2': {'df': {'df1': {'df1': nan}},
+             'files': {'csv': {'csv': 'filename.cvs'},
+                       'pdf': {'pdf': 'filename.pdf'}},
+             'name': {'Value': {'Value': nan}}}}
